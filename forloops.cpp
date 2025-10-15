28078119fbf31e2ee24bcc9e39c5e1aac092bee3
Hrittik Raushan <hrittikraushan9939@gmail.com>
2025-10-15 11:51:37 +0530

    initial commit

---
+++ forloops.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;   
+int main(){
+    int mum;
+    cout<<"enter the number;";
+    cin>>mum;
+    for(int i=1;i<=10;i++)
+    {
+        cout<< mum * i << endl;
+    }
+    return 0;
+
+}
