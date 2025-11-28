1188ae79cfb7e71706a0020bca626fd25a5267d7
Hrittik Raushan <hrittikraushan9939@gmail.com>
2025-11-28 11:54:39 +0530

    initial commit

--- function.cpp
+++ function.cpp
@@ -101,19 +101,69 @@
 //  }
 
 
+//call by value:-
+//write a program to display one integer and one float number using a function.
+
+// #include <iostream>
+// using namespace std;
+// void display(int n1, float n2){
+//     cout<<"The integer number is "<<n1<<endl;
+//     cout<<"The float number is "<<n2<<endl;
+// }
+// int main(){
+//     int num1 = 10;
+//     float num2 = 10.5;
+//     display(num1, num2);
+//     return 0;
+// }
 
 
- //write a program to display one integer and one float number using a function.
+// find area of rectangle using call by value.
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// void area(int l, int b){
+//     cout<<"The area of rectangle is "<<l*b<<endl;
+// }
+// int main(){
+//     int length = 5;
+//     int breadth = 10;
+//     area(length, breadth);
+//     return 0;
+// }
+
+//by using call by value find the area of circle.
+#include <iostream> 
 using namespace std;
-void display(int n1, float n2){
-    cout<<"The integer number is "<<n1<<endl;
-    cout<<"The float number is "<<n2<<endl;
-}
+void area(float r){
+    cout<<"The area of circle is: "<<3.14*r*r<<endl;
+} 
 int main(){
-    int num1 = 10;
-    float num2 = 10.5;
-    display(num1, num2);
+    float radius = 5;
+    area(radius);
     return 0;
 }
+
+
+
+//What is call by address:-
+//ans- call by address is used when we want to modify a variable inside and outside the function.
+//syntex:-
+//return_type function_name(data_type *parameter_name){
+//     //function body or modification
+// }
+
+// #include <iostream>
+// using namespace std;
+// void changevalue (int *p)
+// {
+//     *p = *p+10;
+//     cout<<"Pointervalue:"<<*p<<endl;
+// }
+// int main(){
+//     int num;
+//     cout<<"enter the value:";
+//     cin>>num;
+//     cout <<"original value:"<<num<<endl;
+//     cout<<"new value:"<<num+10<<endl;
+// }
